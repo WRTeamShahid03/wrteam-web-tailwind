93ad1aab066cc2722dc4238b9434afd3b4f5deae
shahid <wrteamshahid03@gmail.com>
2025-06-12 12:00:04 +0530

    DM schema issue fixing

--- src/app/services/digital-marketing/page.tsx
+++ src/app/services/digital-marketing/page.tsx
@@ -70,30 +70,18 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default function Page() {
   const ourJsonLd = {
-    "@context": "https://schema.org",
-    "@type": "Service",
-    "name": "Digital Marketing Services",
+    "@context": "http://schema.org",
+    "@type": "Product",
+    "name": "Digital Marketing Service",
     "description": "Looking for top digital marketing services in Bhuj, India? WRTeam is a leading digital marketing agency and company offering expert internet marketing solutions. Your trusted digital marketing service provider, consultant, and experts in Bhuj.",
-    "serviceType": "Digital Marketing",
-    "provider": {
-      "@type": "Organization",
-      "name": "WRTeam",
-      "url": "https://www.wrteam.in",
-      "logo": {
-        "@type": "ImageObject",
-        "url": "https://www.wrteam.in/_next/static/media/logo.4609846a.svg"
-      }
-    },
-    "areaServed": {
-      "@type": "Place",
-      "name": "India"
-    },
     "url": "https://www.wrteam.in/services/digital-marketing",
-    "image": "https://www.wrteam.in/_next/static/media/Digital_Marketing%20Service.210d5b39.webp",
-    "aggregateRating": {
+    "AggregateRating":
+    {
       "@type": "AggregateRating",
-      "ratingValue": "4.8",
-      "reviewCount": "256"
+      "ratingValue": "4.90",
+      "reviewCount": "110",
+      "worstRating": 1,
+      "bestRating": 5
     }
   };
 
