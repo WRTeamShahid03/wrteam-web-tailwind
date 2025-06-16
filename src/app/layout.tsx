1c083ad3b04aeacd84c6fd983d0eee0f7838d161
shahid <wrteamshahid03@gmail.com>
2025-06-16 13:21:02 +0530

    Update Google Tag Manager implementation: comment out Script component and add inline script for better performance and compatibility.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* <!-- Google Tag Manager --> */}
-        <Script
+        {/* <Script
           strategy="beforeInteractive"
           src="https://www.googletagmanager.com/gtm.js?id=GTM-MG5P53R"
           id="google-tag-manager"
@@ -73,7 +73,12 @@ export default function RootLayout({
                     })(window,document,'script','dataLayer','GTM-MG5P53R')
                 `,
           }}
-        ></Script>
+        ></Script> */}
+         <script 
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MG5P53R');`
+          }}
+        />
         {/* <!-- End Google Tag Manager --> */}
 
         {/* whatsapp widget  */}
