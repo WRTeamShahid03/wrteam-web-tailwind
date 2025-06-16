cd2fee2b0792cde72675a10c0ad9ee57b64c8c14
shahid <wrteamshahid03@gmail.com>
2025-06-16 10:33:54 +0530

    gtm script issue fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,24 +60,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
+      <head>
         {/* <!-- Google Tag Manager --> */}
-        <Script
-          id="gtm-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];
-      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-      var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-      f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MG5P53R');
-    `,
-          }}
-        >
-        </Script>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MG5P53R')
+
+                `}}></script>
         {/* <!-- End Google Tag Manager --> */}
 
         {/* whatsapp widget  */}
@@ -89,7 +81,7 @@ export default function RootLayout({
         >
         </Script>
 
-      </Head>
+      </head>
       <body
         className={`${montserrat.variable} ${poppins.variable} ${roboto.variable} ${archivo.variable} font-sans`}
         suppressHydrationWarning
