df9ec783f333a2123021091ca1429e487171e804
shahid <wrteamshahid03@gmail.com>
2025-08-14 14:07:25 +0530

    Add animation to SaleStripe component: Integrate framer-motion for dynamic visual effects and simplify layout structure

--- src/components/layout/SaleStripe.tsx
+++ src/components/layout/SaleStripe.tsx
@@ -9,6 +9,7 @@ import squareShape from '../../assets/images/stripeLeftImg.svg'
 import grabDeal from '../../assets/images/grapDealBg.png'
 import { RiArrowRightCircleFill } from "react-icons/ri";
 import saleImg from '../../assets/images/saleImg.svg'
+import { motion } from 'framer-motion'
 
 const SaleStripe = ({ setShowSaleStripe }: { setShowSaleStripe: (value: boolean) => void }) => {
 
@@ -76,10 +77,10 @@ const SaleStripe = ({ setShowSaleStripe }: { setShowSaleStripe: (value: boolean)
                 <Image src={circle1} alt='sale' width={0} height={0} className='absolute top-0 left-0 w-auto h-auto  z-[1] hidden lg:block' />
                 <Image src={squareShape} alt='sale' width={0} height={0} className='absolute top-0 right-0 w-auto h-auto  z-[1] hidden lg:block' />
             </div>
-            <div className="max-1680:!container 2xl:max-w-[1620px] mx-auto w-full">
+            <div className="mx-auto w-full">
                 <div className='flex flex-col sm:flex-row flex-wrap sm:flex-nowrap items-center justify-center gap-3 sm:gap-6 md:gap-10 relative z-[2] py-3 between-1200-1399:!py-0 xl:py-0 w-full'>
                     <div className='max-1199:hidden'>
-                    <Image src={saleImg} alt='sale' width={0} height={0} className='w-auto h-auto  z-[1] hidden lg:block' />
+                        <Image src={saleImg} alt='sale' width={0} height={0} className='w-auto h-auto  z-[1] hidden lg:block' />
                     </div>
                     <span className='max-399:text-sm text-[16px] sm:text-[18px] md:text-[22px] between-1200-1399:!text-xl lg:text-[26px] font-extrabold !font-lexend textShadowSale lg:!saleText text-center sm:text-left' data-fill-text="Mid-Year Sale Extended — Last Chance!">Build App & Web at Half Price <span className='max-399:bg-transparent bg-white text-[#2B49DD] px-2 py-1 rounded-md'>50% OFF</span> Freedom SALE is LIVE</span>
                     <div className='flex flex-col sm:flex-row flex-wrap xl:flex-nowrap items-center gap-3 sm:gap-4'>
@@ -88,12 +89,23 @@ const SaleStripe = ({ setShowSaleStripe }: { setShowSaleStripe: (value: boolean)
                             <span className='font-bold text-xs ml-2 sm:ml-0'>Time Only ⌛</span>
                         </div> */}
                         {/* <Image src={line} alt='line' width={0} height={0} className='w-[80px] sm:w-[100px] h-[80px] sm:h-[100px] hidden sm:block' /> */}
-                        <div className='flex !flex-row items-center gap-4'>
+                        <motion.div
+                            className="flex !flex-row items-center gap-4"
+                            animate={{ 
+                                x: [10, -10,10],
+                            }}
+                            transition={{ 
+                                duration: 2,
+                                repeat: Infinity,
+                                ease: "easeInOut"
+                            }}
+                        >
+
                             <div className='relative'>
                                 <Link href={'https://www.wrteam.in/mid-year-sale?utm_source=website&utm_medium=strip&utm_campaign=mid-year-sale'} target='_blank'
                                     className='p-2 md:py-3 sm:px-4 md:px-6 bg-white text-[#364ED2] rounded-full border-2 border-[#ffad3f] text-sm sm:text-base font-semibold !font-lexend whitespace-nowrap flexCenter gap-2'>Grab Deal <RiArrowRightCircleFill size={18} /></Link>
                             </div>
-                        </div>
+                        </motion.div>
                         {
                             isClient && (
                                 <Countdown
