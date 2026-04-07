279f20aaa844a3563c34ac0e827073e0b4ce6c8d
Yash Rajput <yashwrteam89@gmail.com>
2026-04-07 16:19:20 +0530

    feat: set dynamic rendering to force-dynamic for license page

--- src/app/license/page.tsx
+++ src/app/license/page.tsx
@@ -1,6 +1,8 @@
 import License from '@/components/pagesComponent/license/License'
 import React from 'react'
 
+export const dynamic = 'force-dynamic';
+
 const Page = () => {
   return (
     <div><License/></div>
